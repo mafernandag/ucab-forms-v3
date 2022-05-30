ab92197871e21bb013d46980b6a5efb60e0a9861
Román Rodríguez <romanrodri192@gmail.com>
2022-05-30 15:20:44 -0400

    Add firebase environment configuration

--- src/api/firebaseConfig.js
+++ src/api/firebaseConfig.js
@@ -8,13 +8,13 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC8QEkSwKQje02leX_Mh9dEZZduLLQEqw0",
-  authDomain: "ucab-forms.firebaseapp.com",
-  projectId: "ucab-forms",
-  storageBucket: "ucab-forms.appspot.com",
-  messagingSenderId: "674123606931",
-  appId: "1:674123606931:web:7e3a2ccea12cd5cfe41666",
-  measurementId: "G-8CGMPLSZWE",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
