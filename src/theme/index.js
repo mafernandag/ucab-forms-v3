d47a6b954844668771907f2fe05d1f3f7ca47287
Román Rodríguez <romanrodri192@gmail.com>
2022-06-27 14:41:25 -0400

    Add locale to theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,54 +1,58 @@
 import { createTheme } from "@mui/material/styles";
+import { esES } from "@mui/material/locale";
 
 const getTheme = (mode) => {
-  return createTheme({
-    typography: {
-      fontFamily: "Poppins",
-    },
-    components: {
-      ...(mode === "light"
-        ? {
-            MuiAppBar: {
-              styleOverrides: {
-                colorPrimary: {
-                  backgroundColor: "#042F3E",
+  return createTheme(
+    {
+      typography: {
+        fontFamily: "Poppins",
+      },
+      components: {
+        ...(mode === "light"
+          ? {
+              MuiAppBar: {
+                styleOverrides: {
+                  colorPrimary: {
+                    backgroundColor: "#042F3E",
+                  },
                 },
               },
-            },
-            MuiCssBaseline: {
-              styleOverrides: {
-                body: {
-                  backgroundColor: "#fffafa",
+              MuiCssBaseline: {
+                styleOverrides: {
+                  body: {
+                    backgroundColor: "#fffafa",
+                  },
                 },
               },
-            },
-          }
-        : {
-            MuiCssBaseline: {
-              styleOverrides: {
-                body: {
-                  backgroundColor: "#0a0a0a",
+            }
+          : {
+              MuiCssBaseline: {
+                styleOverrides: {
+                  body: {
+                    backgroundColor: "#0a0a0a",
+                  },
                 },
               },
-            },
-          }),
+            }),
+      },
+      palette: {
+        mode,
+        ...(mode === "light"
+          ? {
+              primary: {
+                main: "#13B5EA",
+                contrastText: "#fff",
+              },
+            }
+          : {
+              primary: {
+                main: "#6bc4eb",
+              },
+            }),
+      },
     },
-    palette: {
-      mode,
-      ...(mode === "light"
-        ? {
-            primary: {
-              main: "#13B5EA",
-              contrastText: "#fff",
-            },
-          }
-        : {
-            primary: {
-              main: "#6bc4eb",
-            },
-          }),
-    },
-  });
+    esES
+  );
 };
 
 export default getTheme;
