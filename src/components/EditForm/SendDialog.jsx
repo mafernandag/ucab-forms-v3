b0eacbfe897e1c2bfb63358f86f007e14e2cf42e
Román Rodríguez <romanrodri192@gmail.com>
2022-08-07 22:16:16 -0400

    Improve QR Code

--- src/components/EditForm/SendDialog.jsx
+++ src/components/EditForm/SendDialog.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import {
   Button,
   Dialog,
@@ -6,11 +7,13 @@ import {
   DialogTitle,
   TextField,
   Box,
+  Stack,
 } from "@mui/material";
 import { useSnackbar } from "notistack";
 import { QRCodeSVG } from "qrcode.react";
 
 const SendDialog = ({ open, setOpen }) => {
+  const qrCodeRef = useRef(null);
   const { enqueueSnackbar } = useSnackbar();
 
   const handleClose = () => {
@@ -28,19 +31,84 @@ const SendDialog = ({ open, setOpen }) => {
     }
   };
 
+  const getSvgUrl = (svg) => {
+    const svgData = new XMLSerializer().serializeToString(svg);
+    const blob = new Blob([svgData], { type: "image/svg+xml;charset=utf-8" });
+    const svgUrl = URL.createObjectURL(blob);
+    return svgUrl;
+  };
+
+  const download = (url, fileName) => {
+    const link = document.createElement("a");
+    link.download = fileName;
+    link.href = url;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  const downloadSvg = (svg, fileName) => {
+    const svgUrl = getSvgUrl(svg);
+    download(svgUrl, fileName);
+  };
+
+  const downloadPng = (svg, fileName) => {
+    const canvas = document.createElement("canvas");
+    canvas.width = 128;
+    canvas.height = 128;
+
+    const ctx = canvas.getContext("2d");
+
+    if (ctx) {
+      const svgUrl = getSvgUrl(svg);
+      const img = new Image();
+      img.src = svgUrl;
+
+      img.onload = () => {
+        ctx.drawImage(img, 0, 0);
+        const imgUrl = canvas.toDataURL("image/png");
+        download(imgUrl, fileName);
+      };
+    }
+  };
+
+  const handleDownloadSvg = () => {
+    const svg = qrCodeRef.current?.querySelector("svg");
+    if (svg) {
+      downloadSvg(svg, "QR.svg");
+    }
+  };
+
+  const handleDownloadPng = () => {
+    const svg = qrCodeRef.current?.querySelector("svg");
+    if (svg) {
+      downloadPng(svg, "QR.png");
+    }
+  };
+
   return (
     <Dialog open={open} onClose={handleClose} fullWidth maxWidth="sm">
       <DialogTitle>Enviar Encuesta</DialogTitle>
       <DialogContent>
         <Box
+          ref={qrCodeRef}
           sx={{
             display: "flex",
-            alignItems: "center",
+            alignItems: "flex-end",
             justifyContent: "center",
+            gap: 1,
             mb: 2,
           }}
         >
           <QRCodeSVG value={formUrl} />
+          <Stack>
+            <Button size="small" onClick={handleDownloadPng}>
+              Descargar PNG
+            </Button>
+            <Button size="small" onClick={handleDownloadSvg}>
+              Descargar SVG
+            </Button>
+          </Stack>
         </Box>
         <TextField
           variant="standard"
